b66689991d520a39562446a683442d019c4ee02d
rassafel <rassafel@gmail.com>
2022-10-06 16:50:32 +0400

    Add tests.

---
+++ api/src/test/java/ru/rassafel/todo/controller/ToDoCardControllerTest.java
@@ -0,0 +1,36 @@
+package ru.rassafel.todo.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import ru.rassafel.todo.controller.message.CreateToDoCardRequest;
+
+import java.util.Map;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ToDoCardControllerTest {
+    @Autowired
+    MockMvc mvc;
+    @Autowired
+    ObjectMapper mapper;
+
+    @Test
+    void testCreateCard() throws Exception {
+        mvc.perform(post(ToDoCardController.MAPPING)
+                .content(mapper.writeValueAsString(Map.of(CreateToDoCardRequest.PROPERTY_SUMMARY, "summary",
+                    CreateToDoCardRequest.PROPERTY_DESCRIPTION, "description")))
+                .contentType(MediaType.APPLICATION_JSON))
+            .andDo(MockMvcResultHandlers.print())
+            .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(MockMvcResultMatchers.status().is2xxSuccessful());
+    }
+}
